37231ddac579176c3013602263c3448f118eec95
Alex Kamsteeg <alex@atlex.nl>
2018-06-11 19:26:34 +0200

    Added a throw helper for ArgumentNullExceptions

---
+++ src/Pitcher.Tests/ArgumentNullTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Xunit;
+
+namespace Pitcher.Tests
+{
+  public class ArgumentNullTests
+  {
+    [Fact]
+    public void For_Throws()
+    {
+      Assert.Throws<ArgumentNullException>("TEST", () => Throw.ArgumentNull.For("TEST"));
+    }
+
+    [Fact]
+    public void For_WithMessage_Throws()
+    {
+      Assert.Throws<ArgumentNullException>("TEST", () => Throw.ArgumentNull.For("TEST", "message"));
+    }
+
+    [Fact]
+    public void When_ConditionTrue_Throws()
+    {
+      Assert.Throws<ArgumentNullException>("TEST", () => Throw.ArgumentNull.When(true, "TEST"));
+    }
+
+    [Fact]
+    public void When_WithMessage_ConditionTrue_Throws()
+    {
+      Assert.Throws<ArgumentNullException>("TEST", () => Throw.ArgumentNull.When(true, "TEST", "message"));
+    }
+
+    [Fact]
+    public void When_FuncNull_ThrowsExceptionForWhenParameter()
+    {
+      Assert.Throws<ArgumentNullException>("condition", () => Throw.ArgumentNull.When(null, "TEST"));
+    }
+
+    [Fact]
+    public void When_FuncReturnsTrue_Throws()
+    {
+      Assert.Throws<ArgumentNullException>("TEST", () =>
+        Throw.ArgumentNull.When(() => true, "TEST")
+      );
+    }
+
+    [Fact]
+    public void When_FuncReturnsFalse_DoesNotThrow()
+    {
+      Throw.ArgumentNull.When(() => false, "TEST");
+    }
+
+    [Fact]
+    public void When_WithMessage_FuncNull_ThrowsExceptionForWhenParameter()
+    {
+      Assert.Throws<ArgumentNullException>("condition", () => Throw.ArgumentNull.When(null, "TEST", "message"));
+    }
+
+    [Fact]
+    public void When_WithMessage_FuncReturnsTrue_Throws()
+    {
+      Assert.Throws<ArgumentNullException>("TEST", () =>
+        Throw.ArgumentNull.When(() => true, "TEST", "message")
+      );
+    }
+
+    [Fact]
+    public void When_WithMessage_FuncReturnsFalse_DoesNotThrow()
+    {
+      Throw.ArgumentNull.When(() => false, "TEST", "message");
+    }
+
+    [Fact]
+    public void WhenNull_WithNull_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+        Throw.ArgumentNull.WhenNull(null, "TEST")
+      );
+    }
+
+    [Fact]
+    public void WhenNull_WithObject_DoesNotThrow()
+    {
+      Throw.ArgumentNull.WhenNull(new object(), "TEST");
+    }
+
+    [Fact]
+    public void WhenNull_WithNull_SetCorrectParameterNameOnException()
+    {
+      const string parameterName = "TEST";
+
+      Assert.Throws<ArgumentNullException>(parameterName, () =>
+        Throw.ArgumentNull.WhenNull(null, parameterName)
+      );
+    }
+
+    [Fact]
+    public void WhenNull_WithMessage_WithNull_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+        Throw.ArgumentNull.WhenNull(null, "TEST", "message")
+      );
+    }
+
+    [Fact]
+    public void WhenNull_WithMessage_WithObject_DoesNotThrow()
+    {
+      Throw.ArgumentNull.WhenNull(new object(), "TEST", "message");
+    }
+
+    [Fact]
+    public void WhenNull_WithMessage_WithNull_SetCorrectParameterNameOnException()
+    {
+      const string parameterName = "TEST";
+
+      Assert.Throws<ArgumentNullException>(parameterName, () =>
+        Throw.ArgumentNull.WhenNull(null, parameterName, "message")
+      );
+    }
+
+    [Fact]
+    public void WhenNull_WithMessage_WithNull_SetCorrectMessageOnException()
+    {
+      const string parameterName = "TEST";
+      const string message = "TESTMESSAGE";
+
+      try
+      {
+        Throw.ArgumentNull.WhenNull(null, parameterName, message);
+      }
+      catch (ArgumentNullException e)
+      {
+        Assert.Contains(message, e.Message);
+      }
+    }
+  }
+}
---
+++ src/Pitcher/Throw.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+namespace Pitcher
+{
+  /// <summary>
+  /// Represents a throw helper for exceptions
+  /// </summary>
+  public static class Throw
+  {
+    /// <summary>
+    /// Represents a throw helper for <see cref="ArgumentNullException"/>
+    /// </summary>
+    public static class ArgumentNull
+    {
+      /// <summary>
+      /// Throw an <see cref="ArgumentNullException"/> for the specified
+      /// parameter name
+      /// </summary>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentNullException"/> for
+      /// </param>
+      public static void For(string parameterName) =>
+        throw new ArgumentNullException(parameterName);
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentNullException"/> for the specified
+      /// parameter name
+      /// </summary>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentNullException"/> for
+      /// </param>
+      /// <param name="message">
+      /// The message to include in the <see cref="ArgumentNullException"/>
+      /// </param>
+      public static void For(string parameterName, string message) =>
+        throw new ArgumentNullException(parameterName, message);
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentNullException"/> for the specified
+      /// parameter name when the specified condition is true
+      /// </summary>
+      /// <param name="condition">
+      /// The condition to determine whether to throw the <see cref="ArgumentNullException"/> or not
+      /// </param>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentNullException"/> for
+      /// </param>
+      public static void When(bool condition, string parameterName)
+      {
+        if (condition)
+        {
+          throw new ArgumentNullException(parameterName);
+        }
+      }
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentNullException"/> for the specified
+      /// parameter name when the specified condition is true
+      /// </summary>
+      /// <param name="condition">
+      /// The condition to determine whether to throw the <see
+      /// cref="ArgumentNullException"/> or not
+      /// </param>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentNullException"/> for
+      /// </param>
+      /// <param name="message">
+      /// The message to include in the <see cref="ArgumentNullException"/>
+      /// </param>
+      public static void When(bool condition, string parameterName, string message)
+      {
+        if (condition)
+        {
+          throw new ArgumentNullException(parameterName, message);
+        }
+      }
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentNullException"/> for the specified
+      /// parameter name when the specified <see cref="Func{TResult}"/> returns true
+      /// </summary>
+      /// <param name="condition">
+      /// The <see cref="Func{TResult}"/> to determine whether to throw the <see cref="ArgumentNullException"/> or not
+      /// </param>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentNullException"/> for
+      /// </param>
+      public static void When(Func<bool> condition, string parameterName)
+      {
+        _ = condition ?? throw new ArgumentNullException(nameof(condition));
+
+        var mustThrow = condition.Invoke();
+
+        if (mustThrow)
+        {
+          throw new ArgumentNullException(parameterName);
+        }
+      }
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentNullException"/> for the specified
+      /// parameter name when the specified <see cref="Func{TResult}"/> returns true
+      /// </summary>
+      /// <param name="condition">
+      /// The <see cref="Func{TResult}"/> to determine whether to throw the <see
+      /// cref="ArgumentNullException"/> or not
+      /// </param>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentNullException"/> for
+      /// </param>
+      /// <param name="message">
+      /// The message to include in the <see cref="ArgumentNullException"/>
+      /// </param>
+      public static void When(Func<bool> condition, string parameterName, string message)
+      {
+        _ = condition ?? throw new ArgumentNullException(nameof(condition));
+
+        var mustThrow = condition.Invoke();
+
+        if (mustThrow)
+        {
+          throw new ArgumentNullException(parameterName, message);
+        }
+      }
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentNullException"/> for the specified
+      /// parameter when the specified <see cref="object"/> is null
+      /// </summary>
+      /// <param name="obj">
+      /// The <see cref="object"/> to check for null
+      /// </param>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentNullException"/> for
+      /// </param>
+      public static void WhenNull(object obj, string parameterName)
+      {
+        if (obj == null)
+        {
+          throw new ArgumentNullException(parameterName);
+        }
+      }
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentNullException"/> for the specified
+      /// parameter when the specified <see cref="object"/> is null
+      /// </summary>
+      /// <param name="obj">
+      /// The <see cref="object"/> to check for null
+      /// </param>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentNullException"/> for
+      /// </param>
+      /// <param name="message">
+      /// The message to include in the <see cref="ArgumentNullException"/>
+      /// </param>
+      public static void WhenNull(object obj, string parameterName, string message)
+      {
+        if (obj == null)
+        {
+          throw new ArgumentNullException(parameterName, message);
+        }
+      }
+    }
+
+  }
+}
