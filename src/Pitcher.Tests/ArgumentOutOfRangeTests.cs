44f8e98c5e35a8d214df425f44f1870c5a2b8473
Alex Kamsteeg <alex@atlex.nl>
2018-06-11 19:41:36 +0200

    Added a throw helper for ArgumentOutOfRangeExceptions

---
+++ src/Pitcher.Tests/ArgumentOutOfRangeTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xunit;
+
+namespace Pitcher.Tests
+{
+  public class ArgumentOutOfRangeTests
+  {
+    [Fact]
+    public void For_Throws()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>("TEST", () => Throw.ArgumentOutOfRange.For("TEST"));
+    }
+
+    [Fact]
+    public void For_WithMessage_Throws()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>("TEST", () => Throw.ArgumentOutOfRange.For("TEST", "message"));
+    }
+
+    [Fact]
+    public void When_ConditionTrue_Throws()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>("TEST", () => Throw.ArgumentOutOfRange.When(true, "TEST"));
+    }
+
+    [Fact]
+    public void When_ConditionFalse_DoesNotThrow()
+    {
+      Throw.ArgumentNull.When(false, "TEST");
+    }
+
+    [Fact]
+    public void When_WithMessage_ConditionTrue_Throws()
+    {
+      Assert.Throws<ArgumentOutOfRangeException>("TEST", () => Throw.ArgumentOutOfRange.When(true, "TEST", "message"));
+    }
+
+    [Fact]
+    public void When_WithMessage_ConditionFalse_DoesNotThrow()
+    {
+      Throw.ArgumentNull.When(false, "TEST", "message");
+    }
+  }
+}
--- src/Pitcher/Throw.cs
+++ src/Pitcher/Throw.cs
@@ -171,5 +171,77 @@ namespace Pitcher
       }
     }
 
+    /// <summary>
+    /// Represents a throw helper for see <see cref="ArgumentOutOfRangeException"/>
+    /// </summary>
+    public static class ArgumentOutOfRange
+    {
+      /// <summary>
+      /// Throw an <see cref="ArgumentOutOfRange"/> for the specified
+      /// parameter name
+      /// </summary>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentOutOfRange"/> for
+      /// </param>
+      public static void For(string parameterName) =>
+        throw new ArgumentOutOfRangeException(parameterName);
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentOutOfRange"/> for the specified
+      /// parameter name
+      /// </summary>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentOutOfRange"/> for
+      /// </param>
+      /// <param name="message">
+      /// The message to include in the <see cref="ArgumentOutOfRange"/>
+      /// </param>
+      public static void For(string parameterName, string message) =>
+        throw new ArgumentOutOfRangeException(parameterName, message);
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentOutOfRangeException"/> for the specified
+      /// parameter name when the specified condition is true
+      /// </summary>
+      /// <param name="condition">
+      /// The condition to determine whether to throw the <see cref="ArgumentOutOfRangeException"/> or not
+      /// </param>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentOutOfRangeException"/> for
+      /// </param>
+      public static void When(bool condition, string parameterName)
+      {
+        if (condition)
+        {
+          throw new ArgumentOutOfRangeException(parameterName);
+        }
+      }
+
+      /// <summary>
+      /// Throw an <see cref="ArgumentOutOfRangeException"/> for the specified
+      /// parameter name when the specified condition is true
+      /// </summary>
+      /// <param name="condition">
+      /// The condition to determine whether to throw the <see
+      /// cref="ArgumentOutOfRangeException"/> or not
+      /// </param>
+      /// <param name="parameterName">
+      /// The name of the parameter to throw the <see
+      /// cref="ArgumentOutOfRangeException"/> for
+      /// </param>
+      /// <param name="message">
+      /// The message to include in the <see cref="ArgumentOutOfRangeException"/>
+      /// </param>
+      public static void When(bool condition, string parameterName, string message)
+      {
+        if (condition)
+        {
+          throw new ArgumentOutOfRangeException(parameterName, message);
+        }
+      }
+    }
   }
 }
