d303c5ab90db08aef6fa568164b12e2492767e53
Alex Kamsteeg <alex@atlex.nl>
2019-09-05 18:15:21 +0200

    Split up Throw and Throw<T> into separate files for maintainability

--- src/Pitcher/Throw.cs
+++ src/Pitcher/Throw.cs
@@ -7,54 +7,6 @@ namespace Pitcher
   /// <summary>
   /// Represents a throw helper for exceptions
   /// </summary>
-  /// <typeparam name="TException">
-  /// The type of <see cref="Exception"/> to throw
-  /// </typeparam>
-  public static class Throw<TException>
-    where TException : Exception, new()
-  {
-    /// <summary>
-    /// Throw the <see cref="Exception"/> of the specified type
-    /// </summary>
-    public static void Now()
-    {
-      var ex = Create();
-
-      throw ex;
-    }
-
-    /// <summary>
-    /// Throw the <see cref="Exception"/> of the specified type when the
-    /// specified condition is true
-    /// </summary>
-    /// <param name="condition">
-    /// The condition to determine whether to throw the <see cref="Exception"/>
-    /// or not
-    /// </param>
-    public static void When(bool condition)
-    {
-      if (condition)
-      {
-        Now();
-      }
-    }
-
-    /// <summary>
-    /// Create a new <see cref="Exception"/> of the specified type
-    /// </summary>
-    /// <returns>
-    /// A new <see cref="Exception"/> of the specified type
-    /// </returns>
-    private static TException Create()
-    {
-      var result = Activator.CreateInstance<TException>();
-      return result;
-    }
-  }
-
-  /// <summary>
-  /// Represents a throw helper for exceptions
-  /// </summary>
   public static class Throw
   {
     /// <summary>
@@ -67,7 +19,6 @@ namespace Pitcher
     {
       throw exceptionToThrow;
     }
-
 
     /// <summary>
     /// Throw the specified <see cref="Exception"/> when the specified condition
